e9f947c7a31ed8fa70d94a0111e33b98b6b400f9
Laura Laqui <lauralaqui@icloud.com>
2024-04-17 19:58:57 -0400

    adds image

--- data.js
+++ data.js
@@ -4,21 +4,21 @@ export const menuArray = [
         ingredients: ["shrimp", "cod", "coconut milk", "farofa"],
         id: 0,
         price: 14,
-        image: '/Image/moqueca.svg',
+        image: '/moqueca.svg',
         id: 1
     },
     {
         name: "Pizza",
         ingredients: ["tomato sauce", "cheese", "pepperoni"],
         price: 12,
-        image: '/Image/pizza.svg',
+        image: '/pizza.svg',
         id: 2
     },
         {
         name: "Toro",
         ingredients: ["fatty tuna", "rice", "seaweed", "scallions"],
         price: 12,
-        image: '/Image/toro.svg',
+        image: '/toro.svg',
         id: 3
     }
 ]
